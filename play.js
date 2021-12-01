4af0f645259df041bfb6cb988be1087417850a3b
Josue <arevalo.josued@gmail.com>
2021-12-01 22:35:31 +0000

    play.js setup complete

---
+++ play.js
@@ -0,0 +1,28 @@
+const net = require("net");
+
+//establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",// IP address here,
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", (data) => {
+    // code that does something when the connection is first established
+    console.log(`Connected to server`)
+  });
+  conn.on("data", (data) => {
+    console.log(`data: ${data}`)
+  });
+
+  conn.on("close", (data) => {
+    console.log(`Server closed.`)
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
