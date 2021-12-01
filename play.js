c63f6d1c8e5b8437eb13f3d4f406f3ffbdb18661
Josue <arevalo.josued@gmail.com>
2021-12-01 23:43:48 +0000

    adding in control C to kill

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 const {net, connect} = require("./client.js");
 
-
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (key) { 
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
